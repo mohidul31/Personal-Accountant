deca01cd15995ba21a53c48271a2652242c8f1fa
mohidul31 <mislam6789@gmail.com>
2018-06-24 12:44:36 +0600

    Admin Dashboard Added

---
+++ PersonalAccountant.Web/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalAccountant.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard2()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Blank()
+        {
+            return View();
+        }
+    }
+}
